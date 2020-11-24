becfc6a6c52b42b3a2e6e17298d779b823f3b7dc
Thiback <thibault.delerue@epitech.eu>
2020-11-24 14:21:23 +0100

    [WIP] created the views from step 1. Need to find ways to display it

---
+++ mvvm_post_it_api/src/components/post-it.vue
@@ -0,0 +1,16 @@
+<template>
+<h2>one post it</h2>
+
+</template>
+
+<script>
+    export default {
+        name: 'post_it',
+        props: {
+        msg: String
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ mvvm_post_it_api/src/components/post_it_board.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="displays">
+        <h1>display case</h1>
+        <h2>{{ msg }}</h2>
+        <post_it />
+        <post_it />
+    </div>
+</template>
+
+<script>
+    import post_it from "./post-it.vue";
+    export default {
+        name: 'post_it_board',
+        props: {
+        msg: String,
+        components: {
+            post_it
+        }
+    }
+}
+</script>
+
+<style>
+</style>
